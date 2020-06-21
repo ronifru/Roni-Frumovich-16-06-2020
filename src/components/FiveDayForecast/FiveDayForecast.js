1a888a9a5ef38ca7fc48610091317c27fbd3e7b2
Roni Frumovich <ronifru08@gmail.com>
2020-06-21 14:29:46 +0300

    Revert "Updates"
    This reverts commit 0c825c72b1985c0c54aa63ca103c70fc0a689d4e.

---
+++ src/components/FiveDayForecast/FiveDayForecast.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./FiveDayForecast.scss"
+
+function FiveDayForecast() {
+    return (
+        <div className='five-days-forecast'>
+            <div className='day-forecast'/>
+            <div className='day-forecast'/>
+            <div className='day-forecast'/>
+            <div className='day-forecast'/>
+            <div className='day-forecast'/>
+        </div>
+    );
+}
+
+export default FiveDayForecast;
