0c825c72b1985c0c54aa63ca103c70fc0a689d4e
Roni Frumovich <ronifru08@gmail.com>
2020-06-21 14:20:38 +0300

    Updates

---
+++ precache-manifest.b961999888213057d6248ad07fdb19ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b8908f3939f1098ac13a765b4cd362",
+    "url": "/Roni-Frumovich-16-06-2020/index.html"
+  },
+  {
+    "revision": "6458c15a44d0874e44fe",
+    "url": "/Roni-Frumovich-16-06-2020/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7e4dd69ca49a29299092",
+    "url": "/Roni-Frumovich-16-06-2020/static/css/main.fd8ac8e9.chunk.css"
+  },
+  {
+    "revision": "6458c15a44d0874e44fe",
+    "url": "/Roni-Frumovich-16-06-2020/static/js/2.90e5065a.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/Roni-Frumovich-16-06-2020/static/js/2.90e5065a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4dd69ca49a29299092",
+    "url": "/Roni-Frumovich-16-06-2020/static/js/main.35710847.chunk.js"
+  },
+  {
+    "revision": "16d21a2474a96fca01fb",
+    "url": "/Roni-Frumovich-16-06-2020/static/js/runtime-main.215b41c3.js"
+  }
+]);
